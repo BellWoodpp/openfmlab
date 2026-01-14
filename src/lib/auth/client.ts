40bf330f4ebb6c0f94af3688ac8833e940edf00b
BellWoodpp <stormrobin50@gmail.com>
2026-01-15 07:21:19 +0800

    change auth

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,9 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { magicLinkClient } from "better-auth/client/plugins";
 
-const baseURL = process.env.NEXT_PUBLIC_AUTH_BASE_URL;
-
 export const authClient = createAuthClient({
   plugins: [magicLinkClient()],
-  ...(baseURL ? { baseURL } : {}),
 });
